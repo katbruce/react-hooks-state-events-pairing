807f777d4725a5a5a9277c374fe41643e2534f89
Kathryn <kathrynbruce7@gmail.com>
2023-04-14 12:04:13 -0600

    pair lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import video from "../data/video.js";
+import Header from "./Header.js";
 
 function App() {
-  console.log("Here's your data:", video);
-
+  console.log("Here's your data:", video); 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={video.embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <Header video={video} />
     </div>
+    
   );
 }
 
 export default App;
+//components: video, likes, comments, 
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react"
+import Comments from "./Comments"
+import Votes from "./Votes"
+
+function Header({video}){
+    const [visible, setVisible] = useState(false);
+    
+    // console.log(video.comments)
+    // function commentSection(){  
+    //     const comments = video.comments;
+    //     console.log(comments)
+    //     
+    //         console.log(displayComments);
+    //     return(<ul>{displayComments}</ul>);}
+    let count = 0;
+    const displayComments = video.comments.map((comment) => {
+        count++;
+            return(       
+                <Comments id={comment.id} key={comment.id} user={comment.user} comment={comment.comment} />
+            )})
+
+    return (
+        <div>
+            <h1>{video.title}</h1>
+            <p>{video.views} views | Uploaded {video.createdAt}</p>
+            <Votes />
+            <div>
+                <button onClick={() => setVisible(!visible)}>{visible ? 'Hide Comments' : 'Show Comments'}</button>
+                {visible && <div id="commentsection">
+                <h2>{count} Comments</h2>
+                {displayComments}</div>}
+            </div>
+        </div>
+    )
+}
+    
+
+
+
+
+
+
+export default Header
+
